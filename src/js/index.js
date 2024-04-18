8fce0d35fd5ee3a96f871a4947df182d50355058
antla <antla@ciklum.com>
2024-04-18 11:03:48 +0300

    feat: add page loading; add score

--- src/js/index.js
+++ src/js/index.js
@@ -2,62 +2,65 @@ const SHEET_URL = 'https://script.google.com/macros/s/AKfycbz8_epEb1QAtYaahUEAsS
 
 const clickedLogosArray = [];
 
-const list = document.querySelector('#list');
-const interactiveImage = document.querySelector('.interactive-image');
-const form = document.querySelector('#form');
-const loader = document.querySelector('#loader');
+window.addEventListener('load', function() {
+    const list = document.querySelector('#list');
+    const form = document.querySelector('#form');
+    const loader = document.querySelector('#loader');
+    const scoreEl = document.querySelector('#score');
 
-interactiveImage.addEventListener('load', () => {
-    handleIconsVisible();
-});
-list.addEventListener('click', onClickImg);
-form.addEventListener('submit', onSubmit);
+    list.addEventListener('click', onClickImg);
+    form.addEventListener('submit', onSubmit);
+    handleIconsVisible()
 
+    function handleIconsVisible() {
+        document.querySelectorAll('li').forEach((icon) => icon.style.opacity = 1);
+    };
 
-function handleIconsVisible() {
-    document.querySelectorAll('li').forEach((icon) => icon.style.opacity = 1);
-};
-
-function onClickImg(e) {
-    if (e?.target?.nodeName === 'IMG' && e.target.id) {
-        if (!clickedLogosArray.includes(e.target.id)) {
-            clickedLogosArray.push(e.target.id)
-            e.target.parentNode.style.opacity = 0
+    function onClickImg(e) {
+        if (e?.target?.nodeName === 'IMG' && e.target.id) {
+            if (scoreEl.innerHTML) {
+                scoreEl.innerHTML = ''
+            }
+            if (!clickedLogosArray.includes(e.target.id)) {
+                clickedLogosArray.push(e.target.id)
+                e.target.parentNode.style.opacity = 0
+            }
         }
     }
-}
-function onSubmit(event) {
-    event.preventDefault();
-    const result = clickedLogosArray.length
-    sendDataToGoogleSheet(result);
-    this.reset();
-}
+    function onSubmit(event) {
+        event.preventDefault();
+        const result = clickedLogosArray.length
+        sendDataToGoogleSheet(result);
+        this.reset();
+    }
 
-function sendDataToGoogleSheet(result) {
+    function sendDataToGoogleSheet(result) {
 
-    const formData = new FormData(form)
-    formData.append('result', result)
-    loader.style.display = 'flex'
-    form.style.display = 'none'
+        const formData = new FormData(form)
+        formData.append('result', result)
+        loader.style.display = 'flex'
+        form.style.display = 'none'
 
-    fetch(SHEET_URL, {
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => {
-        if (response.ok) {
-        console.log('Data successfully sent to Google Sheet');
-        clickedLogosArray.length = 0;
-        handleIconsVisible()
-        } else {
-        console.error('Error sending data to Google Sheet');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    })
-    .finally(() => {
-        loader.style.display = 'none'
-        form.style.display = 'flex'
-    });
-}
+        fetch(SHEET_URL, {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => {
+            if (response.ok) {
+            console.log('Data successfully sent to Google Sheet');
+            clickedLogosArray.length = 0;
+            scoreEl.innerHTML = `Score: <b>${result}</b>`
+            handleIconsVisible()
+            } else {
+            console.error('Error sending data to Google Sheet');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+        .finally(() => {
+            loader.style.display = 'none'
+            form.style.display = 'flex'
+        });
+    }
+})
